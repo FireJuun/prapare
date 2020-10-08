b502654ba4b8fed051e9b715540a8bbfd3a2faef
John Manning <john.d.manning@gmail.com>
2020-10-08 10:33:42 -0400

    main screen, initial buttons

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:prapare/views/home.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,9 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Center(
-        child: Text('Barebones'),
-      ),
+      home: Home(),
     );
   }
 }
---
+++ lib/views/home.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFFE4E4E3),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 48.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              _appLogo(),
+              _buttonLarge(title: 'New Survey', onPressed: () {}),
+              _buttonLarge(title: 'Edit Survey'),
+              _buttonLarge(title: 'Submit/Share'),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _appLogo() {
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Image(
+        image: AssetImage('assets/images/PRAPARE-Logo-with-tagline.png'),
+      ),
+    );
+  }
+
+  Widget _buttonLarge({@required String title, Function onPressed}) {
+    return FlatButton(
+      child: SizedBox(
+        width: 300,
+        height: 100,
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Container(
+                width: 280,
+                height: 100,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(22),
+                  color: (onPressed != null)
+                      ? Color(0xFF689E80)
+                      : Color(0xFFB5B5B5),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: Container(
+                width: 280,
+                height: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(22),
+                  color: (onPressed != null)
+                      ? Color(0xFFE1994C)
+                      : Color(0xFFE4E4E3),
+                ),
+                child: Center(
+                    child: Text(
+                  title,
+                  style: TextStyle(fontSize: 36),
+                )),
+              ),
+            ),
+          ],
+        ),
+      ),
+      onPressed: onPressed,
+    );
+  }
+}
