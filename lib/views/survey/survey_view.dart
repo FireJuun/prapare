325f25686e7f7f4f9b85673791d739dadb61588d
John Manning <john.d.manning@gmail.com>
2020-10-25 15:22:28 -0400

    fix: scrolling survey list now changes AppBar todo: re-implement header text/color todo: implement list for each tabview

--- lib/views/survey/survey_view.dart
+++ lib/views/survey/survey_view.dart
@@ -4,128 +4,121 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:get/get.dart';
 import 'package:prapare/controllers/services/theme_service.dart';
-import 'package:prapare/styled_components/app_logo.dart';
 import 'package:prapare/themes.dart';
-import 'package:prapare/views/settings/settings_dialog.dart';
+import 'package:prapare/_internal/components/custom.dart' as custom;
 import 'package:prapare/views/survey/survey_controller.dart';
-import 'package:prapare/views/survey/tabs/home_tab.dart';
-import 'package:prapare/views/survey/tabs/money_tab.dart';
-import 'package:prapare/views/survey/tabs/other_tab.dart';
-import 'package:prapare/views/survey/tabs/personal_tab.dart';
-import 'package:prapare/views/survey/tabs/social_tab.dart';
+import 'package:prapare/views/survey/tabs/shared/personal_characteristics_questions.dart';
 
 class SurveyView extends StatelessWidget {
+  // final double _maxExpandedTabHeight = 120.0;
+  final double _tabHeight = 72.0;
+
   @override
   Widget build(BuildContext context) {
     final AppTheme appTheme = Get.find<ThemeService>()
         .getAppThemeFromBrightness(context.theme.brightness);
 
+    // spec: https://api.flutter.dev/flutter/widgets/NestedScrollView-class.html
+
+    // todo: swap back to getx
     return GetX<SurveyController>(
       builder: (surveyController) {
         final tabList = surveyController.tabModel.tabList;
+        final rxTabIndex = surveyController.rxTabIndex;
+
         return Scaffold(
           backgroundColor: appTheme.bg2,
-          body: CustomScrollView(
-            slivers: [
-              SliverAppBar(
-                title: AppLogoNoTagline(),
-                snap: true,
-                floating: true,
-                pinned: true,
-                actions: [
-                  IconButton(
-                      icon: Icon(Icons.settings),
-                      onPressed: () => settingsDialog())
-                ],
-                bottom: TabBar(
-                  controller: surveyController.tabController,
-                  unselectedLabelColor: appTheme.grey,
-                  labelColor: Colors.black,
-                  tabs: [
-                    /// spread operator used for more concise code
-                    /// obx and rxTabIndex used to trigger redraw on data change
-                    /// function passes index of tabList, which is used by
-                    /// the controller to get the same tabList item
-                    ...List.generate(
-                      tabList.length,
-                      (index) => Tab(
-                        icon: SvgPicture.asset(
-                            surveyController.getTabIconFromIndex(
-                                index, surveyController.rxTabIndex)),
-                      ),
-                    )
-                  ],
-                ),
-              ),
-              SliverPersistentHeader(
-                pinned: true,
-                delegate: _SliverAppBarDelegate(
-                    child: PreferredSize(
-                  preferredSize: Size.fromHeight(80.0),
-                  child: SizedBox(
-                    /// This box also persists under the SliverAppBar widget
-                    /// Using Align and Padding to get around that,
-                    /// though there's probably a more elegant way
-                    height: 100.0,
-                    width: double.infinity,
-                    child: Obx(
-                      () => Container(
-                        color: surveyController.getTabBackgroundColorFromIndex(
-                            surveyController.rxTabIndex, appTheme),
-                        child: Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Padding(
-                              padding: const EdgeInsets.only(bottom: 16.0),
-                              child: Text(
-                                tabList[surveyController.rxTabIndex].title,
-                                style: context.theme.accentTextTheme.headline5,
-                                textAlign: TextAlign.center,
-                              ),
-                            )),
+          body: NestedScrollView(
+            // floatHeaderSlivers: true,
+            headerSliverBuilder:
+                (BuildContext context, bool innerBoxIsScrolled) {
+              // These are the slivers that show up in the "outer" scroll view.
+              return <Widget>[
+                SliverOverlapAbsorber(
+                  handle:
+                      NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                  sliver: SliverAppBar(
+                    excludeHeaderSemantics: true,
+                    title: Text('PRAPARE: ${tabList[rxTabIndex].title}'),
+                    pinned: true,
+                    floating: true,
+                    snap: true,
+                    forceElevated: innerBoxIsScrolled,
+                    bottom: PreferredSize(
+                      /// Preferred size used to match custom tab heights
+                      /// Otherwise, render errors occur when scrolling up
+                      preferredSize: Size.fromHeight(_tabHeight),
+                      child: TabBar(
+                        labelPadding: EdgeInsets.symmetric(
+                            horizontal: 2.0, vertical: 0.0),
+                        controller: surveyController.tabController,
+                        unselectedLabelColor: appTheme.grey,
+                        labelColor: Colors.black,
+                        tabs: [
+                          /// spread operator used for more concise code
+                          /// obx and rxTabIndex used to trigger redraw on data change
+                          /// function passes index of tabList, which is used by
+                          /// the controller to get the same tabList item
+                          ...List.generate(
+                            tabList.length,
+                            (index) => custom.Tab(
+                              // using custom TabBar to set height as a workaround
+                              // issue: https://github.com/flutter/flutter/issues/13322
+                              icon: SvgPicture.asset(
+                                  surveyController.getTabIconFromIndex(
+                                      index, surveyController.rxTabIndex),
+                                  height: _tabHeight),
+                            ),
+                          )
+                        ],
                       ),
                     ),
+                    // todo: implement custom header
+                    // Text(
+                    //   tabList[surveyController.rxTabIndex].title,
+                    //   style: context.theme.accentTextTheme.headline5,
+                    //   textAlign: TextAlign.center,
+                    // ),
                   ),
-                )),
-              ),
-              SliverFillRemaining(
-                child: TabBarView(
-                  controller: surveyController.tabController,
-                  children: [
-                    PersonalTab(),
-                    HomeTab(),
-                    MoneyTab(),
-                    SocialTab(),
-                    OtherTab(),
-                  ],
                 ),
-              )
-            ],
+              ];
+            },
+            body: TabBarView(
+              controller: surveyController.tabController,
+              children: tabList
+                  .map(
+                    (e) => SafeArea(
+                      top: false,
+                      bottom: false,
+                      child: Builder(
+                        builder: (BuildContext context) {
+                          return CustomScrollView(
+                            key: PageStorageKey<String>(e.id.toString()),
+                            slivers: <Widget>[
+                              SliverOverlapInjector(
+                                handle: NestedScrollView
+                                    .sliverOverlapAbsorberHandleFor(context),
+                              ),
+                              SliverPadding(
+                                padding: const EdgeInsets.all(8.0),
+                                sliver: SliverList(
+                                  delegate: SliverChildListDelegate(
+                                    // todo: change list based on active tab
+                                    [PersonalCharacteristicsSurveyQuestions()],
+                                  ),
+                                ),
+                              ),
+                            ],
+                          );
+                        },
+                      ),
+                    ),
+                  )
+                  .toList(),
+            ),
           ),
         );
       },
     );
-  }
-}
-
-class _SliverAppBarDelegate extends SliverPersistentHeaderDelegate {
-  final PreferredSize child;
-
-  _SliverAppBarDelegate({this.child});
-
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return child;
-  }
-
-  @override
-  double get maxExtent => child.preferredSize.height;
-
-  @override
-  double get minExtent => child.preferredSize.height;
-
-  @override
-  bool shouldRebuild(SliverPersistentHeaderDelegate oldDelegate) {
-    return true;
   }
 }
