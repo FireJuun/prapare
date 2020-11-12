5ceeb9a3d83a9d5ed9aa4620b671d75afc67805f
John Manning <john.d.manning@gmail.com>
2020-11-11 21:38:36 -0500

    feat: create single list of all questions to query rather than generating a new list each time the method is called

--- lib/controllers/questionnaire_controller.dart
+++ lib/controllers/questionnaire_controller.dart
@@ -5,29 +5,26 @@ import 'package:prapare/models/data/survey/survey.dart';
 import 'package:prapare/models/questionnaire_model.dart';
 
 class QuestionnaireController extends GetxController {
-  // static QuestionnaireController get to => Get.find();
-
   /// A semi-temporary data model, which will be transitioned to harness [prapareSurvey]
   /// For now, the data points have been created manually, and the codes don't quite correlate yet
 
   final QuestionnaireModel _model = QuestionnaireModel();
+  List<Question> _allQuestions;
 
   //todo: implement error handling / orElse
   Survey getSurveyFromCode(String code) => _model.data.surveys
       .firstWhere((e) => e.code == code, orElse: () => Survey());
 
-  int getTotalIndexFromQuestion(Question question) {
-    final List<Question> tempQuestionList = [
-      ..._model.data.surveys.map((e) => e.questions).expand((x) => x)
-    ];
+  int getTotalIndexFromQuestion(Question question) =>
+      _allQuestions.indexOf(question);
 
-    // todo: implement error handling / duplicates handling
-    return tempQuestionList.indexOf(question);
-  }
+  void _mapAllQuestions() => _allQuestions =
+      _model.data.surveys.map((e) => e.questions).expand((x) => x).toList();
 
   @override
   void onInit() {
     _model.loadAndCreateSurvey();
+    _mapAllQuestions();
     super.onInit();
   }
 }
