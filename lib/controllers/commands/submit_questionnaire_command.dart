96c55819db91bb3c7e72c9ba42913f8a88ff5286
Dokotela <jgfaulkenberry@gmail.com>
2020-12-08 15:22:21 -0500

    Should save locally when press submit

--- lib/controllers/commands/submit_questionnaire_command.dart
+++ lib/controllers/commands/submit_questionnaire_command.dart
@@ -1,8 +1,17 @@
+import 'package:get/get.dart';
+import 'package:prapare/models/fhir_questionnaire/survey/export.dart';
+
+import '../controllers.dart';
 import 'abstract_command.dart';
 
 class SubmitQuestionnaireCommand extends AbstractCommand {
   @override
   Future<void> execute() async {
-    //todo: submit questionnaire
+    final UserResponsesController responsesController = Get.find();
+    final QuestionnaireController questionnaireController = Get.find();
+    final answerListObs = responsesController.userResponsesMap.values;
+    var answerList = <UserResponse>[];
+    answerListObs.forEach((v) => answerList.add(v.value));
+    await questionnaireController.saveResponse(answerList);
   }
 }
