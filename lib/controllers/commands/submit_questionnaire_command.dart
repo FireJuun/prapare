470a15f69fe17c25a8209506ab3174f252a9b6a8
John Manning <45612810+FireJuun@users.noreply.github.com>
2020-12-08 23:16:19 -0500

    Update submit_questionnaire_command.dart

--- lib/controllers/commands/submit_questionnaire_command.dart
+++ lib/controllers/commands/submit_questionnaire_command.dart
@@ -7,8 +7,6 @@ import 'abstract_command.dart';
 class SubmitQuestionnaireCommand extends AbstractCommand {
   @override
   Future<void> execute() async {
-    final UserResponsesController responsesController = Get.find();
-    final QuestionnaireController questionnaireController = Get.find();
     final answerListObs = responsesController.userResponsesMap.values;
     var answerList = <UserResponse>[];
     answerListObs.forEach((v) => answerList.add(v.value));
