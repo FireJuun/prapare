31e171b1e7f7dfa702a175298a49b05f0c7db608
John Manning <john.d.manning@gmail.com>
2020-12-08 23:19:14 -0500

    code cleanup

--- lib/controllers/commands/submit_questionnaire_command.dart
+++ lib/controllers/commands/submit_questionnaire_command.dart
@@ -1,14 +1,12 @@
-import 'package:get/get.dart';
 import 'package:prapare/models/fhir_questionnaire/survey/export.dart';
 
-import '../controllers.dart';
 import 'abstract_command.dart';
 
 class SubmitQuestionnaireCommand extends AbstractCommand {
   @override
   Future<void> execute() async {
     final answerListObs = responsesController.userResponsesMap.values;
-    var answerList = <UserResponse>[];
+    final List<UserResponse> answerList = <UserResponse>[];
     answerListObs.forEach((v) => answerList.add(v.value));
     await questionnaireController.saveResponse(answerList);
   }
