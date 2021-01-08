caf4c5c3199918329980df2cc9eb48a129e101fe
John Manning <john.d.manning@gmail.com>
2021-01-08 00:10:13 -0500

    fix: GetStorage saveBundle now works for iOS too getExternalStorageDirectory method is Android specific

--- lib/services/local_storage.dart
+++ lib/services/local_storage.dart
@@ -4,7 +4,7 @@ import 'package:get_storage/get_storage.dart';
 
 class LocalStorage {
   Future saveBundle(String bundle) async {
-    final path = (await getExternalStorageDirectory()).path;
+    final path = (await getApplicationDocumentsDirectory()).path;
     final file = File('$path/questionnaireBundle.txt');
     file.writeAsString(bundle);
   }
